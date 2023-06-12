5ea79d06e78475eb69192c93ebc166b097a8bd3e
url00 <nathanbrgrn@gmail.com>
2023-06-12 10:56:57 -0500

    Initial commit.

---
+++ Serilog.Sinks.Router/LoggerSinkConfigurationRouterSinkExtensions.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.Extensions.Options;
+using Serilog.Configuration;
+using Serilog.Core;
+using Serilog.Events;
+using Serilog.Sinks.Router.Sinks.Router;
+
+namespace Serilog.Sinks.Router
+{
+    public static class LoggerSinkConfigurationRouterSinkExtensions
+    {
+        public static LoggerConfiguration RouterSink(
+            this LoggerSinkConfiguration loggerSinkConfiguration,
+            IOptionsMonitor<RouterSinkOptions> routerSinkOptionsMonitor,
+            Action<LoggerSinkConfiguration> configureSinkA,
+            Action<LoggerSinkConfiguration> configureSinkB)
+        {
+            ILogEventSink sinkA = null;
+            ILogEventSink sinkB = null;
+            LoggerSinkConfiguration.Wrap(
+                new LoggerConfiguration().WriteTo,
+                s => sinkA = s,
+                configureSinkA,
+                LevelAlias.Minimum,
+                null);
+            LoggerSinkConfiguration.Wrap(
+                new LoggerConfiguration().WriteTo,
+                s => sinkB = s,
+                configureSinkB,
+                LevelAlias.Minimum,
+                null);
+
+
+            var resultSink = new RouterSink(routerSinkOptionsMonitor, sinkA, sinkB);
+            return loggerSinkConfiguration.Sink(resultSink);
+        }
+    }
+}
---
+++ Serilog.Sinks.Router/Sinks/Router/RouterOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serilog.Sinks.Router.Sinks.Router
+{
+    public class RouterSinkOptions
+    {
+        public string ShouldEmitSinkAExpression { get; set; }
+        public string ShouldEmitSinkBExpression { get; set; }
+    }
+}
